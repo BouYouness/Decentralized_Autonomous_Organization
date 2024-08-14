445e3da9e47c816b59b2e5fcd229fd38016174f2
youness <fadisame104@gmail.com>
2024-08-14 12:52:04 +0100

    new commit

---
+++ test/DAOToken.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DAOToken} from "../src/DAOToken.sol";
+
+contract DAOTokenTest is Test {
+    
+}
